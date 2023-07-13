4fb296d2cb77aeb9aa2eab8d5edb018bc0d7d25e
mustafashokry555 <mustafashokry555@gmail.com>
2023-07-13 21:53:01 +0300

    make some modifies

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,51 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// let navButton = document.querySelector(".nav-button");
+  // let navButton = document.querySelector(".nav-button");
 
-// navButton.addEventListener("click", (e) => { 
-//   e.preventDefault();
-  
-//   // toggle nav state
-//   document.body.classList.toggle("nav-visible");
-// });
+  // navButton.addEventListener("click", (e) => { 
+  //   e.preventDefault();
 
-
-$('.toggle-icon .fa-navicon').on('click',function(){
-
-  $('.sidebar').toggleClass('is-visible');
-
-  if ($('.sidebar').hasClass('is-visible')) {
-
-    $('.sidebar').animate({
-      right:0
-    },400)
-
-    $('.box-control').css({
-      display:'block'
-    })
-    
-  }else{
-    $('.sidebar').animate({
-      right:'-250px'
-    },400)
-
-    $('.box-control').css({
-      display:'none'
-    })
-  }
-
-})
+  //   // toggle nav state
+  //   document.body.classList.toggle("nav-visible");
+  // });
 
 
-  $('.box-control').on('click',function(){
+  $('.toggle-icon .fa-navicon').on('click', function () {
+
+    $('.sidebar').toggleClass('is-visible');
+
+    if ($('.sidebar').hasClass('is-visible')) {
+
+      $('.sidebar').animate({
+        right: 0
+      }, 400)
+
+      $('.box-control').css({
+        display: 'block'
+      })
+
+    } else {
+      $('.sidebar').animate({
+        right: '-270'
+      }, 400)
+
+      $('.box-control').css({
+        display: 'none'
+      })
+    }
+
+  })
+
+
+  $('.box-control').on('click', function () {
 
     if ($('.sidebar').hasClass('is-visible')) {
       $('.sidebar').animate({
-        right:'-250px'
-      },400)
+        right: '-270px'
+      }, 400)
 
       $(this).css({
-        display:'none'
+        display: 'none'
       })
     }
 
